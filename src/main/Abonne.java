2e17b599b03e3c7e39bf0a0db0136141603d12cc
Boris THERON <boris.theron@gmail.com>
2026-01-27 14:55:24 +0100

    class creation

--- src/main/Abonne.java
+++ src/main/Abonne.java
@@ -2,4 +2,14 @@ package src.main;
 
 public class Abonne {
 
+    String nom;
+    String prenom;
+    int numeroAbonne;
+
+    public Abonne(String nom, String prenom, int numeroAbonne) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.numeroAbonne = numeroAbonne;
+    }
+
 }
--- src/main/Bibliotheque.java
+++ src/main/Bibliotheque.java
@@ -1,5 +1,27 @@
 package src.main;
 
+import src.Abonne;
+
 public class Bibliotheque {
+
+    public String identification(Abonne abonne) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String reservation() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String emprunt() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String retour() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String rechercher() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
 }
